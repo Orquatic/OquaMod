443165a058d5aff1e1ae6dab24d005c4fbce0003
Orquatic <junior.delaney@outlook.com>
2024-09-17 05:13:05 +1000

    CHANGED SOME THINGS

--- src/main/java/net/orquatic/oquamod/OquaMod.java
+++ src/main/java/net/orquatic/oquamod/OquaMod.java
@@ -1,5 +1,6 @@
 package net.orquatic.oquamod;
 
+import net.orquatic.oquamod.item.Moditems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -51,6 +52,8 @@ public class OquaMod {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        Moditems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -64,7 +67,13 @@ public class OquaMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(Moditems.OQUANITE);}
 
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(Moditems.RAW_OQUANITE);
+
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/orquatic/oquamod/item/Moditems.java
@@ -0,0 +1,22 @@
+package net.orquatic.oquamod.item;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.orquatic.oquamod.OquaMod;
+
+public class Moditems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(OquaMod.MOD_ID);
+
+    public static final DeferredItem<Item> OQUANITE = ITEMS.register("oquanite",
+            () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> RAW_OQUANITE = ITEMS.register("raw_oquanite",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}

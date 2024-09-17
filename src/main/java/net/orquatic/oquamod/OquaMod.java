5f81ffe8e8f1a9d3bab07c5f7211c223fe16b1f4
Orquatic <junior.delaney@outlook.com>
2024-09-17 17:30:34 +1000

    Added: - Creativemode tabs (Item/Block. - Created smelting and blasting for the raw_oquanite item turns into oquanite. - Created oquanite block recipe. (vise versa)

--- src/main/java/net/orquatic/oquamod/OquaMod.java
+++ src/main/java/net/orquatic/oquamod/OquaMod.java
@@ -1,6 +1,7 @@
 package net.orquatic.oquamod;
 
 import net.orquatic.oquamod.block.Modblocks;
+import net.orquatic.oquamod.item.ModCreativeModeTabs;
 import net.orquatic.oquamod.item.Moditems;
 import org.slf4j.Logger;
 
@@ -52,6 +53,8 @@ public class OquaMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         Moditems.register(modEventBus);
         Modblocks.register(modEventBus);
--- src/main/java/net/orquatic/oquamod/item/ModCreativeModeTabs.java
+++ src/main/java/net/orquatic/oquamod/item/ModCreativeModeTabs.java
@@ -1,14 +1,38 @@
 package net.orquatic.oquamod.item;
 
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.orquatic.oquamod.OquaMod;
+import net.orquatic.oquamod.block.Modblocks;
+
+import java.util.function.Supplier;
 
 public class ModCreativeModeTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, OquaMod.MOD_ID);
+
+    public static final Supplier<CreativeModeTab> OQUAMOD_ITEMS_TAB = CREATIVE_MODE_TAB.register("oquamod_items_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(Moditems.OQUANITE.get()))
+                    .title(Component.translatable("creativetab.oquamod.oquamod_items"))
+                    .displayItems((ItemDisplayParameters, output)-> {
+                        output.accept(new ItemStack(Moditems.OQUANITE.get()));
+                        output.accept(new ItemStack(Moditems.RAW_OQUANITE.get()));
+                    }).build());
+
+        public static final Supplier<CreativeModeTab> OQUAMOD_BLOCK_TAB = CREATIVE_MODE_TAB.register("oquamod_blocks_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(Modblocks.OQUANITE_ORE))
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath(OquaMod.MOD_ID, "oquamod_items_tab"))
+                    .title(Component.translatable("creativetab.oquamod.oquamod_blocks"))
+                    .displayItems((ItemDisplayParameters, output)-> {
+                        output.accept(new ItemStack(Modblocks.OQUANITE_ORE.get()));
+                        output.accept(new ItemStack(Modblocks.OQUANITE_BLOCK.get()));
+                    }).build());
+
 
     public static void register(IEventBus eventBus) {
         CREATIVE_MODE_TAB.register(eventBus);

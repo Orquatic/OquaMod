90d15cd652a20ff311d96a0eedb1c4569c2accb7
Orquatic <junior.delaney@outlook.com>
2024-09-28 00:28:15 +1000

    new stuff. stuck on coin counter. unknown resource location issue.

--- src/main/java/net/orquatic/oquamod/OquaMod.java
+++ src/main/java/net/orquatic/oquamod/OquaMod.java
@@ -1,118 +1,41 @@
 package net.orquatic.oquamod;
 
-import net.neoforged.neoforge.client.event.RenderGuiLayerEvent;
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
 import net.orquatic.oquamod.block.Modblocks;
+import net.orquatic.oquamod.gui.KeyBindings;
 import net.orquatic.oquamod.item.ModCreativeModeTabs;
 import net.orquatic.oquamod.item.Moditems;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-
 import org.slf4j.Logger;
-
 import com.mojang.logging.LogUtils;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-
-
-
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(OquaMod.MOD_ID)
 public class OquaMod {
     public static final String MOD_ID = "oquamod";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public OquaMod(IEventBus modEventBus, ModContainer modContainer) {
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
+        // Register mod events
+        modEventBus.addListener(this::setup);
 
-        // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
-        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
-        NeoForge.EVENT_BUS.register(this);
-
+        // Register items, blocks, and creative tabs
+        Moditems.ITEMS.register(modEventBus);
+        Modblocks.BLOCKS.register(modEventBus);
         ModCreativeModeTabs.register(modEventBus);
 
-        // Register event listeners and item registration
-        Moditems.ITEMS.register(modEventBus);
+        // Register key bindings
+        KeyBindings.register(modEventBus);
 
-        Moditems.register(modEventBus);
-        Modblocks.register(modEventBus);
-        KronaCounterGui.onRenderGuiOverlay((RenderGuiLayerEvent.Post) modEventBus);
-
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
-
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+        // Register server events
+        NeoForge.EVENT_BUS.register(this);
     }
 
-
-
-    private void commonSetup(final FMLCommonSetupEvent event) {
-
-    }
-
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
-            event.accept(Moditems.OQUANITE);
-            event.accept(Moditems.RAW_OQUANITE);
-            event.accept(Moditems.SICKLE);
-            event.accept(Moditems.KRONA);
-            event.accept(Moditems.OQUANITE_INGOT);
-        }
-
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS){
-            event.accept(Modblocks.OQUANITE_BLOCK);
-            event.accept(Modblocks.OQUANITE_ORE);
-        }
-    }
-
-        // You can use SubscribeEvent and let the Event Bus discover methods to call
-        @SubscribeEvent
-        public void onServerStarting (ServerStartingEvent event){
-
-        }
-
-        // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-        @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-        public static class ClientModEvents {
-            @SubscribeEvent
-            public static void onClientSetup(FMLClientSetupEvent event) {
-        }
-
+    private void setup(final FMLClientSetupEvent event) {
+        LOGGER.info("Setup complete for {}", OquaMod.MOD_ID);
     }
 }
-
